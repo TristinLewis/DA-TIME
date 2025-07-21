786b47263ef204e813527f11ad4f8b5bfaf85c3a
Tristin Lewis <TLEWIS20@cub.uca.edu>
2025-07-21 11:24:45 -0500

    Starting of DA assignment, 7/21, TIME.

--- DA TIME/Time.cpp
+++ DA TIME/Time.cpp
@@ -1,1 +1,40 @@
 #include "Time.h"
+#include <iostream> 
+
+
+Time::Time() {
+
+}
+Time::Time(int days, int hours, int months) {
+
+}
+Time::Time(int hours, int minutes, int seconds) {
+
+}
+
+void Time::setDays() {
+
+}
+void Time::setHours() {
+
+}
+void Time::setMinutes() {
+
+}
+void Time::setSeconds() {
+
+}
+
+int Time::getDays() {
+
+}
+int Time::getHours() {
+
+}
+int Time::getMinutes() {
+
+}
+int Time::getSeconds() {
+
+}
+
--- DA TIME/Time.h
+++ DA TIME/Time.h
@@ -1,5 +1,28 @@
-#pragma once
-class Time
-{
+#ifndef Time_h
+#define Time_h
+class Time{
+private:
+	int days, hours, minutes, seconds; 
+public:
+	Time();
+	Time(int days, int hours, int months);
+	Time(int hours, int minutes, int seconds);
+
+	void setDays();
+	void setHours();
+	void setMinutes();
+	void setSeconds();
+
+	int getDays();
+	int getHours();
+	int getMinutes();
+	int getSeconds();
+
+
+
+
+
 };
+#endif
+
 
